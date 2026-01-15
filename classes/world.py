fdb0e1f799dc1f4e4ecc491bfac0a77c1eb7b97f
carbondioxide22 <kariarnason06@gmail.com>
2026-01-15 11:11:45 +0000

    lalala

--- classes/world.py
+++ classes/world.py
@@ -13,6 +13,9 @@ class World(object):
 
         for x in range(1, self.__width + 1):
             for y in range(1, self.__height + 1):
-                tile_list.append(Tile(x, y))
+                tile_list.append(Tile((x, y), "nesw"))
 
         return tile_list
+    
+    def __generate_path(self):
+        pass
