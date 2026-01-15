28608dcf47f9c63b04933cdc988a1bf949895f23
carbondioxide22 <kariarnason06@gmail.com>
2026-01-15 10:41:04 +0000

    World class

--- classes/world.py
+++ classes/world.py
@@ -1,3 +1,18 @@
+from tile import Tile
+import random
+
 class World(object):
-    def __init__(self):
-        pass
+    def __init__(self, width: int=3, height: int=3):
+        self.__width = width
+        self.__height = height
+
+        self.__tile_list = self.__generate_map()
+
+    def __generate_map(self):
+        tile_list = list()
+
+        for x in range(1, self.__width + 1):
+            for y in range(1, self.__height + 1):
+                tile_list.append(Tile(x, y))
+
+        return tile_list
